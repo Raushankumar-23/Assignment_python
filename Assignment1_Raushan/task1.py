d6eb3bd61eb164eb78bc40ba36c1db0ca44c7a2c
Raushankumar-23 <raushan.engineer23@gmail.com>
2026-02-18 00:49:28 +0530

    Initial commit Assignment complete

---
+++ Assignment1_Raushan/task1.py
@@ -0,0 +1,21 @@
+
+"""
+Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+
+
+num1=eval(input("Enter first number : "))
+num2=eval(input("Enter first number : "))
+
+print(f"Addition of {num1} and {num2} is ",num1+num2)
+print(f"Subtraction of {num1} minus {num2} is ",num1-num2)
+print(f"Multiplication of {num1} and {num2} is ",num1*num2)
+print(f"Division of {num1} divide by {num2} is ",num1/num2)
---
+++ Assignment1_Raushan/task2.py
@@ -0,0 +1,22 @@
+"""
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+
+"""
+from tkinter.font import names
+
+s1=input("Enter your first name : ")
+s2=input("Enter your last name : ")
+
+print(f"hello, {s1} {s2} ! welcome to python program")
+
+
+
+
+
+
+
+
