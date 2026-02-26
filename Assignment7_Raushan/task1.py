52c7168e5d91d18c33b6a61c9fc684a8f3c33300
Raushankumar-23 <raushan.engineer23@gmail.com>
2026-02-27 01:51:02 +0530

    Add files via upload

--- Assignment7_Raushan/task1.py
+++ Assignment7_Raushan/task1.py
@@ -1,12 +1,218 @@
+# python connected with database
+# import psycopg2
+# connect = psycopg2.connect(dbname="postgres",user="postgres",password="PGRaushan@123",host="localhost",port="5432")
+# print("connected Successfully")
+
+
+# create table
+
+# import psycopg2
+# conn = psycopg2.connect(dbname="postgres",user="postgres",password="PGRaushan@123",host="localhost",port="5432")
+
+# cursor = conn.cursor()
+# cursor.execute('''create table employees(Name Text,ID int, Age int);''')
+# print("Table created successfully")
+
+# conn.commit()
+# conn.close()
+
+
+
 """
-ASSIGNMENT 7:
-Module 16: Building Database Apps with PostgreSQL & Python
-Description:  For this assignment, you are required to:
-Complete all course lectures within the module, ensuring you follow the concepts taught in each session.
-Implement all practical exercises provided in the course lectures. Each practical task should be executed and the results properly documented.
-Take complete, unedited screenshots of each practical output along with the corresponding code. Ensure that the following are visible in your screenshots:
-The full code you executed.
-The output displayed on your system, including the system timestamp.
-No cropping or editing should be done on the screenshots. Any attempt to crop or modify the screenshots will result in your submission being considered invalid, and you will be required to resubmit it.
+# insert daata
+
+import psycopg2
+
+# Create table
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="PGRaushan@123",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS employees(
+    Name TEXT,
+    ID INT,
+    Age INT
+);
+''')
+
+print("Table created successfully")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+# Insert data function
+def data():
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="PGRaushan@123",
+        host="localhost",
+        port="5432"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    INSERT INTO employees(Name, ID, Age)
+    VALUES ('Raushan', 1, 21);
+    ''')
+
+    print("Data added successfully")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+# Call function
+data()
+
 """
 
+
+"""
+# add data
+
+
+import psycopg2
+
+# Create table
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="PGRaushan@123",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS employees(
+    Name TEXT,
+    ID INT,
+    Age INT
+);
+''')
+
+print("Table created successfully")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+# Insert data function
+def data():
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="PGRaushan@123",
+        host="localhost",
+        port="5432"
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute('''
+    INSERT INTO employees(Name, ID, Age)
+    VALUES ('Raushan', 1, 21);
+    ''')
+
+    print("Data added successfully")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def extract():
+    conn = psycopg2.connect(dbname="postgres",user="postgres",password="PGRaushan@123",host="localhost",port="5432")
+    
+    
+    cursor = conn.cursor()
+    cursor.execute('''select * from employees;''')
+    show=cursor.fetchone()
+    print(show[0]) #index
+    #print("Table created successfully")
+    
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+extract()
+
+"""
+
+
+
+# user input
+
+
+import psycopg2
+
+# Create table
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="PGRaushan@123",
+    host="localhost",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS employees(
+    Name TEXT,
+    ID INT,
+    Age INT
+);
+''')
+
+print("Table created successfully")
+
+conn.commit()
+cursor.close()
+conn.close()
+
+
+# Insert data function
+def data():
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user="postgres",
+        password="PGRaushan@123",
+        host="localhost",
+        port="5432"
+    )
+
+    cursor = conn.cursor()
+    
+    name = input("Enter name : ")
+    id = input("Enter id : ")
+    age = input("Enter age : ")
+    
+    
+    query=('''INSERT INTO employees(Name, ID, Age)VALUES (%s,%s,%s);''')
+    cursor.execute(query,(name,id,age))
+
+    print("Data added successfully")
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+data()
