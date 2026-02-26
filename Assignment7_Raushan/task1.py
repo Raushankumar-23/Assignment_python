5ac401bdb8709246ecadb5e5bc7b8ea4286ad216
Raushankumar-23 <raushan.engineer23@gmail.com>
2026-02-27 01:49:22 +0530

    hello

---
+++ Assignment7_Raushan/task1.py
@@ -0,0 +1,12 @@
+"""
+ASSIGNMENT 7:
+Module 16: Building Database Apps with PostgreSQL & Python
+Description:  For this assignment, you are required to:
+Complete all course lectures within the module, ensuring you follow the concepts taught in each session.
+Implement all practical exercises provided in the course lectures. Each practical task should be executed and the results properly documented.
+Take complete, unedited screenshots of each practical output along with the corresponding code. Ensure that the following are visible in your screenshots:
+The full code you executed.
+The output displayed on your system, including the system timestamp.
+No cropping or editing should be done on the screenshots. Any attempt to crop or modify the screenshots will result in your submission being considered invalid, and you will be required to resubmit it.
+"""
+
