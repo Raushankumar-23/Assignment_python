b2f0e0209799cc3921ed0bb0f5f2a5754ba50e91
Raushankumar-23 <raushan.engineer23@gmail.com>
2026-02-27 01:40:07 +0530

    your message

--- Assignment6_Raushan/task1.py
+++ Assignment6_Raushan/task1.py
@@ -1,3 +1,21 @@
+""""
+ASSIGNMENT 6:
+Module 14 & 15: CALCULATOR USING TKINTER
+
+Submission: You need to upload the compressed zip file of your working project folder to drive and then submit the accessible link (make sure to enable sharing access).
+Reference: For reference, as mentioned above, please follow Python course >Module 14&15.
+
+To know how to zip the file exactly, follow the below steps:
+Right click on the project folder
+Click on send to
+Then on compressed zip
+"""
+
+
+
+
+
+
 # step-1 importing
 
 import tkinter as tk
